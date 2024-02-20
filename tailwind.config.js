06e3e76d9c07cfba5f33819a8eadb255596b1053
Sinan Babakishiyev <86959839+SinanBabakishiyev@users.noreply.github.com>
2024-02-20 17:46:12 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        renktext: '#004e9c',
+      }
+    },
+    
+  },
+  plugins: [],
+}
